4eed12c7786e970aec67a874f8b4b85a59031a44
liubo@zkp2p.com <123456>
2017-02-04 17:13:19 +0800

    add   prototype  pattern

---
+++ pattern-learning/src/main/java/com/comenie/pattern/creational/prototype/Mage.java
@@ -0,0 +1,13 @@
+package com.comenie.pattern.creational.prototype;
+
+/**
+ * 
+ * Mage
+ *
+ */
+public abstract class Mage extends Prototype {
+
+	@Override
+	public abstract Mage clone() throws CloneNotSupportedException;
+
+}
---
+++ pattern-learning/src/main/java/com/comenie/pattern/creational/prototype/Warlord.java
@@ -0,0 +1,13 @@
+package com.comenie.pattern.creational.prototype;
+
+/**
+ * 
+ * Warlord
+ *
+ */
+public abstract class Warlord extends Prototype {
+
+	@Override
+	public abstract Warlord clone() throws CloneNotSupportedException;
+
+}
