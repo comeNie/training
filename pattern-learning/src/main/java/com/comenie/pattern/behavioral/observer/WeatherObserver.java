6af1390fe723af3b5d5be2ac0d5b259f11f366cc
liubo@zkp2p.com <123456>
2017-02-06 16:44:45 +0800

    add observer pattern

---
+++ pattern-learning/src/main/java/com/comenie/pattern/behavioral/observer/WeatherObserver.java
@@ -0,0 +1,12 @@
+package com.comenie.pattern.behavioral.observer;
+
+/**
+ * 
+ * Observer interface.
+ * 
+ */
+public interface WeatherObserver {
+
+	void update(WeatherType currentWeather);
+
+}
---
+++ pattern-learning/src/main/java/com/comenie/pattern/behavioral/observer/generic/GWeather.java
@@ -0,0 +1,25 @@
+package com.comenie.pattern.behavioral.observer.generic;
+
+
+import com.comenie.pattern.behavioral.observer.WeatherType;
+
+/**
+ * 
+ * GWeather
+ *
+ */
+public class GWeather extends Observable<GWeather, Race, WeatherType> {
+
+    private WeatherType currentWeather;
+
+    public GWeather() {
+        currentWeather = WeatherType.SUNNY;
+    }
+
+    public void timePasses() {
+        WeatherType[] enumValues = WeatherType.values();
+        currentWeather = enumValues[(currentWeather.ordinal() + 1) % enumValues.length];
+        System.out.println("The weather changed to " + currentWeather + ".");
+        notifyObservers(currentWeather);
+    }
+}
