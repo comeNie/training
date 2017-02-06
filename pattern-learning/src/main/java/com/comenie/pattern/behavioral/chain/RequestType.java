6a6fd142297f1017833289031d44f1efa7cdc290
liubo@zkp2p.com <123456>
2017-02-06 09:22:57 +0800

    add chain pattern

---
+++ pattern-learning/src/main/java/com/comenie/pattern/behavioral/chain/RequestType.java
@@ -0,0 +1,12 @@
+package com.comenie.pattern.behavioral.chain;
+
+/**
+ * 
+ * RequestType enumeration
+ *
+ */
+public enum RequestType {
+
+	DEFEND_CASTLE, TORTURE_PRISONER, COLLECT_TAX
+
+}
---
+++ pattern-learning/src/main/java/com/comenie/pattern/behavioral/chain/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Created by 波 on 2017/2/6.
+ */
+package com.comenie.pattern.behavioral.chain;
+/**
+ * 职责链模式(Chain of Responsibility Pattern)：避免请求发送者与接收者
+ * 耦合在一起，让多个对象都有可能接收请求，将这些对象连接成一条链，并且沿着这条链传
+ * 递请求，直到有对象处理它为止。职责链模式是一种对象行为型
+ *
+ *
+ */
