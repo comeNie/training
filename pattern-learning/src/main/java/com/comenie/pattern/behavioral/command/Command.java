b6a2a88343643da44b91ff506c7ada4b3158e4a5
liubo@zkp2p.com <123456>
2017-02-06 10:28:47 +0800

    add command pattern

---
+++ pattern-learning/src/main/java/com/comenie/pattern/behavioral/command/Command.java
@@ -0,0 +1,19 @@
+package com.comenie.pattern.behavioral.command;
+
+/**
+ * 
+ * Interface for Commands.
+ * 
+ */
+public abstract class Command {
+
+	public abstract void execute(Target target);
+
+	public abstract void undo();
+
+	public abstract void redo();
+
+	@Override
+	public abstract String toString();
+
+}
