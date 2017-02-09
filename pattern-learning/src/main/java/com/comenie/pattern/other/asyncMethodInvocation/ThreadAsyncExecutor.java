5610ff8b7ce15687fd39a3750eb4c71327974f26
liubo@zkp2p.com <123456>
2017-02-09 11:40:21 +0800

    add async method invocation pattern

---
+++ pattern-learning/src/main/java/com/comenie/pattern/other/asyncMethodInvocation/ThreadAsyncExecutor.java
@@ -0,0 +1,121 @@
+package com.comenie.pattern.other.asyncMethodInvocation;
+
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+/**
+ * Created by 波 on 2017/2/9.
+ */
+public class ThreadAsyncExecutor  implements  AsyncExecutor{
+    /** Index for thread naming */
+    private final AtomicInteger idx = new AtomicInteger(0);
+
+    @Override
+    public <T> AsyncResult<T> startProcess(Callable<T> task) {
+        return startProcess(task,null);
+    }
+
+    @Override
+    public <T> AsyncResult<T> startProcess(Callable<T> task, AsyncCallback<T> callback) {
+        CompletableResult<T> result = new CompletableResult<>(callback);
+        new Thread(() -> {
+            try {
+                result.setValue(task.call());
+            } catch (Exception ex) {
+                result.setException(ex);
+            }
+        }, "executor-" + idx.incrementAndGet()).start();
+        return result;
+    }
+
+    @Override
+    public <T> T endProcess(AsyncResult<T> asyncResult) throws ExecutionException, InterruptedException {
+        if (asyncResult.isCompleted()) {
+            return asyncResult.getValue();
+        } else {
+            asyncResult.await();
+            return asyncResult.getValue();
+        }
+    }
+
+    private static class CompletableResult<T> implements AsyncResult<T>{
+
+        /**
+         * thread state
+         */
+        static final int RUNNING = 1;
+        static final int FAILED = 2;
+        static final int COMPLETED = 3;
+
+        final  Object lock;
+        final Optional<AsyncCallback<T>> callback;
+
+        volatile int state = RUNNING;
+        T value;
+        Exception exception;
+
+        CompletableResult(AsyncCallback<T> callback) {
+            this.lock = new Object();
+            this.callback = Optional.ofNullable(callback);
+        }
+
+        void setValue(T value){
+            this.value = value;
+            this.state = COMPLETED;
+            this.callback.ifPresent(new Consumer<AsyncCallback<T>>() {
+                @Override
+                public void accept(AsyncCallback<T> tAsyncCallback) {
+                    tAsyncCallback.onComplete(value,Optional.<Exception>empty());
+                }
+            });
+            synchronized (lock) {
+                lock.notifyAll();
+            }
+        }
+
+        void  setException(Exception exception){
+            this.exception = exception;
+            this.state = FAILED;
+            this.callback.ifPresent(new Consumer<AsyncCallback<T>>() {
+                @Override
+                public void accept(AsyncCallback<T> tAsyncCallback) {
+                    tAsyncCallback.onComplete(null,Optional.of(exception));
+                }
+            });
+
+            synchronized (lock) {
+                lock.notifyAll();
+            }
+        }
+
+
+
+        @Override
+        public boolean isCompleted() {
+            return (state > RUNNING);
+        }
+
+        @Override
+        public T getValue() throws ExecutionException {
+            if (state == COMPLETED) {
+                return value;
+            } else if (state == FAILED) {
+                throw new ExecutionException(exception);
+            } else {
+                throw new IllegalStateException("Execution not completed yet");
+            }
+        }
+
+        @Override
+        public void await() throws InterruptedException {
+            synchronized (lock) {
+                if (!isCompleted()) {
+                    lock.wait();
+                }
+            }
+        }
+    }
+}
